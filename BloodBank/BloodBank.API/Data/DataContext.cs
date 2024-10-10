7747f963eba6285ce733f8874fb858face671e2f
dzuluaga23 <david12-zuluaga@hotmail.com>
2024-10-09 19:27:21 -0500

    DataContext

---
+++ BloodBank/BloodBank.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+
+namespace BloodBank.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
