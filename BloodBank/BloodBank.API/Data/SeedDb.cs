7caf9fcfa8cd855e17a6c1fc14282de59b7dace9
dzuluaga23 <david12-zuluaga@hotmail.com>
2024-10-11 21:11:00 -0500

    seedDb

---
+++ BloodBank/BloodBank.API/Data/SeedDb.cs
@@ -0,0 +1,35 @@
+﻿using BloodBank.Shared.Entidades;
+
+namespace BloodBank.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckInventarioAsync();
+        }
+
+        public async Task CheckInventarioAsync()
+        {
+            if (!_context.Inventarios.Any())
+            {
+                _context.Inventarios.Add(new Inventario { Tipo = "A+" });
+                _context.Inventarios.Add(new Inventario { Tipo = "O+" });
+                _context.Inventarios.Add(new Inventario { Tipo = "B+" });
+                _context.Inventarios.Add(new Inventario { Tipo = "AB+" });
+                _context.Inventarios.Add(new Inventario { Tipo = "A-" });
+                _context.Inventarios.Add(new Inventario { Tipo = "O-" });
+                _context.Inventarios.Add(new Inventario { Tipo = "B-" });
+                _context.Inventarios.Add(new Inventario { Tipo = "AB-" });
+            }
+            await _context.SaveChangesAsync();
+        }
+    }
+}
