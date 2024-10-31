be5b378239ef053ae850b18238836eb2d89c13f6
dzuluaga23 <david12-zuluaga@hotmail.com>
2024-10-31 17:06:44 -0500

    Web citas, donantes y donaciones

--- BloodBank/BloodBank.WEB/Repositories/HttpResponseWrapper.cs
+++ BloodBank/BloodBank.WEB/Repositories/HttpResponseWrapper.cs
@@ -2,48 +2,87 @@
 
 namespace BloodBank.WEB.Repositories
 {
-        public class HttpResponseWrapper<T>
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+
         {
-            public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
-            {
-                Error = error;
-                Response = response;
-                HttpResponseMessage = httpResponseMessage;
-            }
 
-            public bool Error { get; set; }
+            Error = error;
 
-            public T? Response { get; set; }
+            Response = response;
 
-            public HttpResponseMessage HttpResponseMessage { get; set; }
+            HttpResponseMessage = httpResponseMessage;
 
-            public async Task<string?> GetErrorMessageAsync()
-            {
-                if (!Error)
-                {
-                    return null;
-                }
-
-                var codigoEstatus = HttpResponseMessage.StatusCode;
-                if (codigoEstatus == HttpStatusCode.NotFound)//404
-                {
-                    return "Recurso no encontrado";
-                }
-                else if (codigoEstatus == HttpStatusCode.BadRequest)//400
-                {
-                    return await HttpResponseMessage.Content.ReadAsStringAsync();
-                }
-                else if (codigoEstatus == HttpStatusCode.Unauthorized)//401
-                {
-                    return " Debes loguearte para realizar esta acción";
-                }
-                else if (codigoEstatus == HttpStatusCode.Forbidden)//403
-                {
-                    return " No tienes permisos para ejecutar esta acción";
-                }
-
-                return "Ha ocurrido un error inesperado";
-            }
         }
-    }
 
+
+
+        public bool Error { get; set; }
+
+
+
+        public T? Response { get; set; }
+
+
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+
+
+        public async Task<string?> GetErrorMessageAsync()
+
+        {
+
+            if (!Error)
+
+            {
+
+                return null;
+
+            }
+
+
+
+            var codigoEstatus = HttpResponseMessage.StatusCode;
+
+            if (codigoEstatus == HttpStatusCode.NotFound)//404 
+
+            {
+
+                return "Recurso no encontrado";
+
+            }
+
+            else if (codigoEstatus == HttpStatusCode.BadRequest)//400 
+
+            {
+
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+
+            }
+
+            else if (codigoEstatus == HttpStatusCode.Unauthorized)//401 
+
+            {
+
+                return " Debes loguearte para realizar esta acción";
+
+            }
+
+            else if (codigoEstatus == HttpStatusCode.Forbidden)//403 
+
+            {
+
+                return " No tienes permisos para ejecutar esta acción";
+
+            }
+
+
+
+            return "Ha ocurrido un error inesperado";
+
+        }
+
+    }
+}
